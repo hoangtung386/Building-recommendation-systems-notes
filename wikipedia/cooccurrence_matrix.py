3a9165cdc9e6e0063beaf082afda11563449d3b3
hoangtung386 <hoangtung386@gmail.com>
2026-06-09 14:47:41 +0700

    Refactor wikipedia/cooccurrence_matrix.py: tf.data → PyTorch Dataset

--- wikipedia/cooccurrence_matrix.py
+++ wikipedia/cooccurrence_matrix.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#
+
 """
     Cooccurrence matrix library.
 """
@@ -13,7 +12,9 @@ from random import shuffle
 
 import nlp_pb2 as nlp_pb
 import numpy as np
-import tensorflow as tf
+import torch
+from torch.utils.data import IterableDataset, DataLoader
+
 
 class CooccurrenceMatrix:
 
@@ -36,12 +37,9 @@ class CooccurrenceMatrix:
                 break
 
     def load(self, input_file):
-        """Loads a dictionary."""
         self.__reset()
-        # List of co-occurrence of token i with token j.
         with bz2.open(input_file, 'rb') as file:
             for line in file:
-                # Drop the trailing \n
                 line = line[:-1]
                 serialized = base64.b64decode(line)
                 proto = nlp_pb.CooccurrenceRow()
@@ -54,13 +52,13 @@ class CooccurrenceMatrix:
     def __init__(self, input_file):
         self.load(input_file)
 
+
 class CooccurrenceGenerator:
     def __init__(self, input_pattern):
         self._input_files = glob.glob(input_pattern)
         self._total_files = len(self._input_files)
 
     def get_item(self):
-        """Gets a single item of i, j, count"""
         while True:
             file_epoch = 0
             for input_file in self._input_files:
@@ -68,7 +66,6 @@ class CooccurrenceGenerator:
                 print('Opening %s (%d of %d)' % (input_file, file_epoch, self._total_files))
                 with bz2.open(input_file, 'rb') as file:
                     for line in file:
-                        # Drop the trailing \n
                         line = line[:-1]
                         serialized = base64.b64decode(line)
                         proto = nlp_pb.CooccurrenceRow()
@@ -78,7 +75,6 @@ class CooccurrenceGenerator:
                             yield (proto.index, proto.other_index[i], proto.count[i])
 
     def get_shuffled_items(self, num_items):
-        """Pre-fetches and shuffles num_items of stuff."""
         iterator = self.get_item()
         while True:
             items = [next(iterator) for _ in range(num_items)]
@@ -105,11 +101,26 @@ class CooccurrenceGenerator:
             y = np.asarray(token_count, dtype=np.float32)
             yield (x, y)
 
-    def get_dataset(self, batch_size, shuffle_size=0):
-        """Returns data as a tensorflow dataset."""
-        ds = tf.data.Dataset.from_generator(
-                lambda : self.get_batch(batch_size, shuffle_size),
-        output_signature=(
-            tf.TensorSpec(shape=(2, batch_size), dtype=tf.int32),
-            tf.TensorSpec(shape=(batch_size), dtype=tf.float32)))
-        return ds
+
+class CooccurrenceIterableDataset(IterableDataset):
+    def __init__(self, generator, batch_size, shuffle_size=0):
+        self._generator = generator
+        self._batch_size = batch_size
+        self._shuffle_size = shuffle_size
+
+    def __iter__(self):
+        batch_iter = self._generator.get_batch(self._batch_size, self._shuffle_size)
+        while True:
+            try:
+                x, y = next(batch_iter)
+                yield (
+                    torch.tensor(x[0], dtype=torch.long),
+                    torch.tensor(x[1], dtype=torch.long),
+                ), torch.tensor(y, dtype=torch.float32)
+            except StopIteration:
+                break
+
+
+def create_dataloader(generator, batch_size, shuffle_size=0, num_workers=0):
+    dataset = CooccurrenceIterableDataset(generator, batch_size, shuffle_size)
+    return DataLoader(dataset, batch_size=None, num_workers=num_workers, pin_memory=True)
