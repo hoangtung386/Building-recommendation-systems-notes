d4221dab86aef5fc114679a55f30641fc14b8f29
hoangtung386 <hoangtung386@gmail.com>
2026-06-09 14:48:02 +0700

    Refactor wikipedia/debug_callbacks.py: Keras callbacks → PyTorch functions

--- wikipedia/debug_callbacks.py
+++ wikipedia/debug_callbacks.py
@@ -1,70 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#
 
 """
-  Debugging callbacks for Keras.
+  Debugging utilities for PyTorch models.
 """
 
-import tensorflow as tf
-from tensorflow.keras.callbacks import Callback
 import numpy as np
-from tensorflow.keras import backend as K
-from tensorflow.keras.models import Model
+import torch
 
 
-class WordNNCallback(Callback):
-    """Nearest neighbor callback."""
-    def __init__(self, csv, max_terms, token_dictionary):
-        super(Callback, self).__init__()
-        tokens = csv.split(',')
-        self.tokens = []
-        self.indices = []
-        self.num_embeddings = token_dictionary.get_embedding_dictionary_size()
-        self.max_terms = max_terms
-        for token in tokens:
-            index = token_dictionary.get_embedding_index(token)
-            self.tokens.append(token)
-            self.indices.append(index)
-        self.indices = np.asarray(self.indices, dtype=np.int32)
-        self.indices_as_tensors = tf.convert_to_tensor(self.indices)
-        self.token_dictionary = token_dictionary
+def dump_word_knn(model, tokens, token_dictionary, max_terms, device):
+    model.eval()
+    with torch.no_grad():
+        tokens = torch.tensor(tokens, dtype=torch.long, device=device)
+        target_embeddings = model.word_embedding(tokens)
+        all_indices = torch.arange(model.word_embedding.num_embeddings, device=device)
+        all_embeddings = model.word_embedding(all_indices)
+        results = torch.matmul(target_embeddings, all_embeddings.T)
+        results = results.cpu().numpy()
 
-    def on_epoch_end(self, epoch, logs=None):
-        embedding_model = Model(inputs=self.model.get_layer("token").input,
-                                outputs=self.model.get_layer("word_embedding").output)
-        all_indices = np.array(np.arange(self.num_embeddings), dtype=np.int32)
-        all_indices = tf.convert_to_tensor(all_indices)
-        embeddings = embedding_model(all_indices)
-        target_embeddings = embedding_model(self.indices_as_tensors)
-        # Find distances from all target embeddings to all other embeddings.
-        results = K.dot(target_embeddings, K.transpose(embeddings))
-        results = K.get_session().run(results)
-        count = min(self.max_terms, self.num_embeddings)
-        for i in range(len(self.tokens)):
+        num_embeddings = model.word_embedding.num_embeddings
+        count = min(max_terms, num_embeddings)
+        for i in range(len(tokens)):
             far_to_near_indices = np.argsort(results[i])
             result_list = []
             for j in range(count):
-                idx = far_to_near_indices[self.num_embeddings - 1 - j]
+                idx = far_to_near_indices[num_embeddings - 1 - j]
                 sim = results[i][idx]
-                other_token = self.token_dictionary.get_token_from_embedding_index(idx)
+                other_token = token_dictionary.get_token_from_embedding_index(idx)
                 display = '%s:%3f' % (other_token, sim)
                 result_list.append(display)
-            print('Nearest to %s: %s' % (self.tokens[i], ','.join(result_list)))
+            print('Nearest to %s: %s' % (token_dictionary.get_token_from_embedding_index(tokens[i].item()),
+                                          ','.join(result_list)))
 
 
-class SentenceNNCallback(Callback):
-    """Sentence nearest neighbor callback."""
-    def __init__(self, csv, max_sentence_length, max_terms, token_dictionary, title_dictionary):
-        super(Callback, self).__init__()
-        self.sentences = csv.split(',')
-        self.tokens = []
-        self.indices = []
-        self.num_embeddings = title_dictionary.get_dictionary_size()
-        self.max_terms = max_terms
+def dump_sentence_knn(model, sentences, max_sentence_length, max_terms,
+                      token_dictionary, title_dictionary, device):
+    model.eval()
+    with torch.no_grad():
         indices = []
-        for sentence in self.sentences:
+        for sentence in sentences:
             tokens = token_dictionary.simple_tokenize(sentence)
             idx = token_dictionary.get_embedding_indices(tokens)
             if len(idx) > max_sentence_length:
@@ -73,32 +48,26 @@ class SentenceNNCallback(Callback):
                 while len(idx) < max_sentence_length:
                     idx.append(0)
             indices.append(idx)
+        indices = torch.tensor(indices, dtype=torch.long, device=device)
 
-        self.indices = np.asarray(indices, dtype=np.int32)
-        self.indices_as_tensors = tf.convert_to_tensor(self.indices)
-        self.title_dictionary = title_dictionary
+        sentence_embeddings = model.encode_sentence(indices)
 
-    def on_epoch_end(self, epoch, logs=None):
-        sentence_model = Model(inputs=self.model.get_layer("sentence_input").input,
-                               outputs=self.model.get_layer("sentence_to_url").output)
-        url_model = Model(inputs=self.model.get_layer("url_near_text").input,
-                          outputs=self.model.get_layer("url_embedding").output)
-        all_indices = np.array(np.arange(self.num_embeddings), dtype=np.int32)
-        all_indices = tf.convert_to_tensor(all_indices)
-        url_embeddings = url_model(all_indices)
-        target_embeddings = sentence_model(self.indices_as_tensors)
-        distances = -K.dot(target_embeddings, K.transpose(url_embeddings))
-        distances = K.get_session().run(distances)
-        count = min(self.max_terms, self.num_embeddings)
-        for i in range(len(self.sentences)):
-            print('Nearest to %s' % self.sentences[i])
-            # Find distances from all target embeddings to all other embeddings.
+        num_embeddings = title_dictionary.get_dictionary_size()
+        all_indices = torch.arange(num_embeddings, device=device)
+        url_embeddings = model.url_embedding(all_indices)
+
+        distances = -torch.matmul(sentence_embeddings, url_embeddings.T)
+        distances = distances.cpu().numpy()
+
+        count = min(max_terms, num_embeddings)
+        for i in range(len(sentences)):
+            print('Nearest to %s' % sentences[i])
             near_to_far_indices = np.argsort(distances[i])
             result_list = []
             for j in range(count):
                 idx = near_to_far_indices[j]
                 sim = -distances[i][idx]
-                other_token = self.title_dictionary.get_token(idx)
+                other_token = title_dictionary.get_token(idx)
                 display = '%s:%3f' % (other_token, sim)
                 result_list.append(display)
-            print('Nearest to %s: %s' % (self.sentences[i], ','.join(result_list)))
+            print('Nearest to %s: %s' % (sentences[i], ','.join(result_list)))
